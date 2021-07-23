03a1ccd9bf73c54cd61122c47de754c7ac841556
Will Lehman <william.p.lehman@usace.army.mil>
2021-07-23 12:50:29 -0700

    updating banner image and alt text.

--- src/containers/main/banner.js
+++ src/containers/main/banner.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'redux-bundler-react';
-import mapper from '../../resources/cm.png';
+import mapper from '../../resources/GeoFDA_Icon.ico';
 class Banner extends React.Component{
     render(){
         return (
             <nav className="navbar navbar=expand-lg navbar-dark bg-dark">
                 <div className="float-left">
-                    <a href = "/"><img src={mapper} alt="USACE LOGO" style={{width:'45px'}}/></a>
+                    <a href = "/"><img src={mapper} alt="NSI LOGO" style={{width:'36px'}}/></a>
                     <a className="navbar-brand" href="/" style={{paddingLeft:"15px", fontSize:'25px'}}><b>National Structure Inventory</b></a>           
                 </div>
             </nav>
