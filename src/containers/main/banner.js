3688035e1126c83198b3298d1070f5c3156770e4
Triet Nguyen <triet.nguyen@usace.army.mil>
2021-07-26 02:14:54 +0000

    added help link to nav banner

--- src/containers/main/banner.js
+++ src/containers/main/banner.js
@@ -9,6 +9,16 @@ class Banner extends React.Component{
                     <a href = "/"><img src={mapper} alt="NSI LOGO" style={{width:'36px'}}/></a>
                     <a className="navbar-brand" href="/" style={{paddingLeft:"15px", fontSize:'25px'}}><b>National Structure Inventory</b></a>           
                 </div>
+                <div className="nav pull-right">
+                    <a 
+                        // type="button" 
+                        class="btn btn-secondary" // gray outline
+                        href="https://www.hec.usace.army.mil/fwlink/?linkid=nsi-download-tool"
+                        target="_blank" // open in new tab
+                        style={{fontSize:'10px'}}>
+                        Help
+                    </a>
+                </div>
             </nav>
         )
     }
