9e715281e9340c9a89d0fac6636ba139de07a9f5
Triet Nguyen <trietpersonal@gmail.com>
2021-08-17 17:45:01 +0000

    removed download button from PR, incorporated state management for popup / robust routing

--- src/app-bundles/index.js
+++ src/app-bundles/index.js
@@ -8,10 +8,12 @@ import cache from '@corpsmap/corpsmap/src/utils/cache';
 
 import routesBundle from './routes-bundle';
 import authBundle from './auth-bundle';
+import downloadBundle from '../cm-plugins/download/cm3-download-bundle';
 
 export default composeBundles(
   routesBundle,
   authBundle,
+  downloadBundle,
   createUrlBundle(),
   createCacheBundle(cache.set),
 );
