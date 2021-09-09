ac3af7855e6d63602b5070bf2b2371b2f1e9894b
Triet Nguyen <trietpersonal@gmail.com>
2021-09-09 02:44:25 +0000

    added cwbi logo; centered nsi logo

--- src/containers/main/banner.js
+++ src/containers/main/banner.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import { connect } from 'redux-bundler-react';
 import mapper from '../../resources/GeoFDA_Icon.ico';
+import cwbiLogo from "../../resources/CWBI_Logo_160x160.png";
 import './main.css';
+import classes from "../login/LoginBanner.module.css";
 
-class Banner extends React.Component{
-    render(){
-        return (
-            <nav className="navbar navbar=expand-lg navbar-dark bg-dark">
-                <div className="float-left">
-                    <a href = "/"><img src={mapper} alt="NSI LOGO" style={{width:'36px'}}/></a>
-                    <a className="navbar-brand" href="/" style={{paddingLeft:"15px", fontSize:'25px'}}><b>National Structure Inventory</b></a>           
-                </div>
-                <div className="nav pull-right">
-                    <a 
-                        className="btn btn-secondary" // gray outline
-                        href="https://www.hec.usace.army.mil/fwlink/?linkid=nsi-download-tool"
-                        target="_blank" // open in new tab
-                        style={{fontSize:'10px'}}>
-                        Help
-                    </a>
-                </div>
-            </nav>
-        )
-    }
+
+class Banner extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar=expand-lg navbar-dark bg-dark nv-bg">
+        <div className="nav float-left">
+          <a href=""><img src={mapper} alt="NSI LOGO" style={{ width: '45px' }} /></a>
+          <a className="navbar-brand" href="" style={{ paddingLeft: "15px", fontSize: '25px' }}><b>National Structure Inventory</b></a>
+        </div>
+        <div className="nav pull-right">
+
+          <a
+            className="btn btn-secondary" // gray outline
+            href="https://www.hec.usace.army.mil/fwlink/?linkid=nsi-download-tool"
+            target="_blank" // open in new tab
+            style={{ fontSize: '1rem' }}>
+            Help
+          </a>
+
+          <img className={classes["cwbi-logo"]} title="Powered By Civil Works Business Intelligence" src={cwbiLogo} alt="CWBI LOGO" />
+
+        </div>
+      </nav>
+    )
+  }
 }
 export default connect(
-    Banner
+  Banner
 );
--- src/containers/map/map-page.js
+++ src/containers/map/map-page.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { connect } from 'redux-bundler-react';
 import {
   Map,
-  addData,
   basemapSwitcher,
   coordDisplay,
-  draw,
-  treeView,
   geocoder,
   identify,
   measureTools,
@@ -18,37 +15,34 @@ import {
 } from "@corpsmap/corpsmap";
 import "@corpsmap/corpsmap/css/corpsmap.css";
 import download from '../../cm-plugins/download';
-import nsiVTL from '../../cm-plugins/vtl/index';
+// import nsiVTL from '../../cm-plugins/vtl/index'; // future feature
 
 class MapPage extends React.Component {
-  render(){
+  render() {
     return (
-        <div className="container-fluid" style={{ padding: "0"}}>
-            <Map
-              theme="grey"
-              plugins={[
-                addData,
-                basemapSwitcher,
-                coordDisplay,
-                draw,
-                treeView(),
-                geocoder,
-                identify,
-                measureTools,
-                rotateNorth,
-                zoomInOut,
-                zoomHome,
-                zoomToBox,
-                mapLoadingIndicator,
-                download,
-                nsiVTL,
-              ]}
-            />        
-        </div>
+      <div className="container-fluid" style={{ padding: "0" }}>
+        <Map
+          theme="grey"
+          plugins={[
+            basemapSwitcher,
+            coordDisplay,
+            geocoder,
+            identify,
+            measureTools,
+            rotateNorth,
+            zoomInOut,
+            zoomHome,
+            zoomToBox,
+            mapLoadingIndicator,
+            download,
+            // nsiVTL, // future feature
+          ]}
+        />
+      </div>
     )
   }
 }
 export default connect(
   MapPage
-  );
+);
 
